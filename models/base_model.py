1e85afa6eb619eaac4bb211f102214a60ff6a1f5
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-09 15:20:36 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,7 @@
 
 import uuid
 from datetime import datetime
-from . import storage
-
+import models as md
 
 class BaseModel:
     """Defines all common attributes/methods for other classes """
@@ -21,7 +20,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+            md.storage.new(self)
 
     def __str__(self):
         """Modifies how its printed"""
@@ -30,7 +29,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        md.storage.save()
 
     def to_dict(self):
         """returns a dictionary of __dict__ of instance"""
