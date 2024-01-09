dd1322a73bcedd843eaa7e976a8c720d5057e694
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-09 21:29:47 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 import os
 import json
 
@@ -12,7 +17,7 @@ class FileStorage:
     """Serializes json file to instances and vice-versa """
 
     __file_path = "file.json"
-    class_dict = {"BaseModel": BaseModel, "User": User}
+    class_dict = {"BaseModel": BaseModel, "User": User, "State": State, "City":City, "Amenity":Amenity, "Place": Place, "Review":Review}
     __objects = {}
 
     def all(self):
