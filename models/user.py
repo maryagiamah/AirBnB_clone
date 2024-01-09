e17b8a37d3d67655d2978164248ac11e23a0ba63
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-01-09 17:35:20 +0100

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains User Model"""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+  """ User Model"""
+  self.email = ""
+  self.password = ""
+  self.first_name = ""
+  self.last_name = ""
+
+  def __init__(self, *args,**kwargs):
+    
