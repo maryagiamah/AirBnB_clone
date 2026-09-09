83721fbc21b3a1e16ff2ca5ef15542e011e39355
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-09-09 01:45:11 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""Test base model"""
 import unittest
+from ../models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test Base Model"""
+    def 
+
+if __name__ == '__main__':
+    unittest.main()
