a2946e0e4f869878a928effaeb782f3f9736437e
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-09-09 02:08:40 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,12 +1,26 @@
-#!/usr/bin/python3
-"""Test base model"""
 import unittest
-from ../models.base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
     """Test Base Model"""
-    def 
+    def setUp(self):
+        self.model = BaseModel()
+
+    def tearDown(self):
+        del self.model
+
+    def test_save(self):
+        self.model.name = "My First Model"
+        self.model.id = 89
+        self.model.save()
+
+    def test_toDict(self):
+        self.model.to_dict()
+
+    def test_id(self):
+        self.assertEqual(self.model.id, 89)
+
 
 if __name__ == '__main__':
     unittest.main()
