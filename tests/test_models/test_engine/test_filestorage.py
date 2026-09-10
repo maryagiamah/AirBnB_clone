c7ad0a5d8a3682b0c22f3b2ea1adbc36f229d38b
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-09-10 12:32:20 +0100

    Update test_filestorage.py

--- tests/test_models/test_engine/test_filestorage.py
+++ tests/test_models/test_engine/test_filestorage.py
@@ -1,2 +1,11 @@
 import unittest
-from models.engine.file_storage
+from models.engine import file_storage
+
+
+Class Test_FileStorage(unittest.Testcase):
+    def setUpClass(cls):
+        storage = file_storage.FileStorage()
+        storage.reload()
+
+    def tearDownClass(cls):
+        del self.model
