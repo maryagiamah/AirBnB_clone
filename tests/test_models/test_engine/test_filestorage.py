06237f098d9d3cce94219df03e8ffa1b407034b7
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-09-14 00:55:52 +0100

    Update test_filestorage.py

--- tests/test_models/test_engine/test_filestorage.py
+++ tests/test_models/test_engine/test_filestorage.py
@@ -6,8 +6,34 @@ from models.engine import file_storage
 Class Test_FileStorage(unittest.Testcase):
     def setUpClass(cls):
         cls.storage = file_storage.FileStorage()
+        cls.file_path = cls.storage._FileStorage__file_path
+        cls.objects = cls.storage._FileStorage__objects
 
     def tearDownClass(cls):
         if os.path.exists(cls.file_path):
             os.remove(cls.file_path)
-    def test_file
+
+    def test_filePath(self):
+        self.assertIsNotNone(cls.file_path)
+        self.assertIsInstance(cls.file_path, string)
+
+    def test_objects(self):
+        self.assertIsInstance(cls.objects, dict)
+
+    def test_all(self):
+        self assertIs(cls.objects, cls.storage.all())
+        self.assertEqual(cls.objects, {})
+
+    def test_new(self):
+        self.model = BaseModel()
+        cls.storage.new(self.model)
+        obj_key = f"BaseModel.{self.model.id}"
+        self.assertIn(obj_key, cls.objects.keys())
+
+    def test_save(self):
+       self.model = BaseModel()
+       cls.storage.save()
+       self.assertTrue(os.path.exists(cls.file_path))
+
+    def test_reload(self):
+       cls.storage.reload() 
