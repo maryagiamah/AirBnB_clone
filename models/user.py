520e236226a5ec96db922ebfa029681e41ac7c67
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-09-14 02:38:30 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -6,8 +6,8 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-  """ User Model"""
-  email = ""
-  password = ""
-  first_name = ""
-  last_name = ""
+    """ User Model"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
