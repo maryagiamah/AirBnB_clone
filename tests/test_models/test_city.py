9357ead61fc11b3c73da850d0ec541385ed455a9
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-09-18 00:41:34 +0100

    Create test_city.py

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,29 @@
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+
+    @class_method
+    def setUpClass(cls):
+        cls.user = User()
+
+    @class_method
+    def tearDownClass(cls):
+        del cls.user
+
+    def test_userEmail(self):
+        self.assertIsInstance(self.user.email, str)
+
+    def test_userPassword(self):
+        self.assertIsInstance(self.user.password, str)
+
+    def test_userLastName(self):
+        self.assertIsInstance(self.user.lastname, str)
+
+    def test_userFirstName(self):
+        self.assertIsInstance(self.user.firstname, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
